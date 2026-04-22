fdb18cb6683c5a4c246aa8d1f880f73f7f2eeb65
Yohannes Taddesse <yohannesshewit1@gmail.com>
2026-04-22 16:14:22 +0300

    Improve comments in getMovies function
    Updated comments for clarity on API URL selection and search parameters.

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,10 @@ async function getMovies(query: string) {
   const url = query
     ? `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${query}`
     : `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.TMDB_API_KEY}`;
+//query is true ,url = search/movie API      query is false, url = trending/movie API
 
-  const res = await fetch(url, { cache: "no-store" });
+  
+  const res = await fetch(url, { cache: "no-store" });  //Fresh data is fetched ,Makes search dynamic
   const data = await res.json();
 
   return data?.results ?? [];
@@ -17,8 +19,8 @@ export default async function Home({
 }: {
   searchParams: Promise<{ query?: string }>;
 }) {
-  const params = await searchParams;
-  const query = params?.query || "";
+  const params = await searchParams;  //searchParams is likely coming from props in Next.js (App Router).
+  const query = params?.query || "";   //If params exists, get query. If not, return undefined."
 
   const movies = await getMovies(query);
 
