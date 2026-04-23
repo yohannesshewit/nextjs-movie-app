e45b2709dc0779c843c949434a78166d66428a71
Yohannes Taddesse <yohannesshewit1@gmail.com>
2026-04-23 14:50:24 +0300

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -2,3 +2,31 @@ const API_URL = "https://api.themoviedb.org/3";
 const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
 
 export { API_URL, IMAGE_PATH };
+
+
+{/*
+u can use at component image section like this 
+
+
+<Image
+  src={
+    movie.poster_path
+      ? `${IMAGE_PATH}${movie.poster_path}`
+      : "/fallback.jpg"
+  }
+/>
+
+
+*/}
+
+{/*  simple way better
+export const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
+
+export const getImageUrl = (path: string | null) =>
+  path ? `${IMAGE_PATH}${path}` : "/fallback.jpg";
+
+then write like this in component 
+
+<Image src={getImageUrl(movie.poster_path)} />
+
+*/}
